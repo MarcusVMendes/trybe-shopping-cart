b6947a782ea399a8353f94acc1c51080c743b1e8
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-07-13 11:52:56 -0300

    Envia requisito 1 funcionando

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+// Função que cria cada section de itens
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  return section;
+}
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-//   return section;
-// }
+const appendChildSection = () => {
+  const section = document.querySelector('.items');
+  console.log(section);
+};
+appendChildSection();
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -32,6 +37,7 @@
 //   // coloque seu código aqui
 // }
 
+// Funçao de adicionar ao carrinho
 // function createCartItemElement({ sku, name, salePrice }) {
 //   const li = document.createElement('li');
 //   li.className = 'cart__item';
@@ -40,4 +46,32 @@
 //   return li;
 // }
 
-window.onload = () => { };
+// Requisito 1 - Fazendo a requisição da API
+
+const objectProduct = (object) => {
+  const sectionContainer = document.querySelector('.items');
+  object.results.forEach((product) => {
+    const newObject = {
+      sku: product.id,
+      name: product.title,
+      image: product.thumbnail,
+      salePrice: product.prices,
+    };
+    console.log(createProductItemElement(newObject));
+
+    return sectionContainer.appendChild(createProductItemElement(newObject));
+  });
+};
+
+const fetchProducts = (product) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((object) => {
+      objectProduct(object);
+    });
+};
+
+window.onload = () => { 
+  fetchProducts('computador');
+};
