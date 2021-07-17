817ae73775bf1f0a49c4c5f974c178f1fd1277fd
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-07-17 16:41:43 -0300

    Requisito 2 funcionando

--- script.js
+++ script.js
@@ -11,6 +11,33 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  console.log(event);  
+}
+
+// Funçao de adicionar ao carrinho
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// Requisito 2 - Adicionar itens ao carrinho.
+
+const fetchProductsByID = async (event) => {
+  const id = event.target.parentElement.firstChild.innerText;
+  const url = `https://api.mercadolibre.com/items/${id}`;
+  const response = await fetch(url);
+  const object = await response.json();
+  const cart = document.querySelector('.cart__items');
+  
+  return cart.appendChild(createCartItemElement(object));
+};
+
 // Função que cria cada section de itens
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -19,37 +46,18 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', fetchProductsByID);
   return section;
 }
-
-const appendChildSection = () => {
-  const section = document.querySelector('.items');
-  console.log(section);
-};
-appendChildSection();
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// Funçao de adicionar ao carrinho
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-
-// Requisito 1 - Fazendo a requisição da API
-
+// Requisito 1 - Fazendo a requisição da API e criando as Seções
 const objectProduct = (object) => {
-  const sectionContainer = document.querySelector('.items');
+  const sectionItems = document.querySelector('.items');
   object.results.forEach((product) => {
     const newObject = {
       sku: product.id,
@@ -57,20 +65,17 @@ const objectProduct = (object) => {
       image: product.thumbnail,
       salePrice: product.prices,
     };
-    console.log(createProductItemElement(newObject));
-
-    return sectionContainer.appendChild(createProductItemElement(newObject));
+    return sectionItems.appendChild(createProductItemElement(newObject));
   });
 };
 
-const fetchProducts = (product) => {
+const fetchProducts = async (product) => {
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((object) => {
-      objectProduct(object);
-    });
+  const response = await fetch(url);
+  const object = await response.json();
+  objectProduct(object);
 };
+//
 
 window.onload = () => { 
   fetchProducts('computador');
